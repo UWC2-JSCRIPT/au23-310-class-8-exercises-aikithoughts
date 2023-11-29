ab10ad2bb174568f9b0c0ce086983aa7a29a9c72
Dave Shevitz <shevitz@stripe.com>
2023-11-28 19:54:21 -0800

    initial homework assignment 8

---
+++ fetch-api-key.js
@@ -0,0 +1,2 @@
+const API_KEY="OIlNCJZgXuwdGxUGJPkZyLGOaKFN9wWA"
+//const API_KEY="S2hsh3Cs8Yyyua4q"
---
+++ hello-darkness.js
@@ -0,0 +1,11 @@
+let rgb = 255;
+let darkenBackground = setInterval(() => {
+    if (rgb > 0) {
+        document.body.style.backgroundColor = `rgb(${rgb}, ${rgb}, ${rgb})`;
+        rgb--;
+        
+    } else {
+        clearInterval(darkenBackground);
+        console.log("Done!");
+    }
+}, 100)
